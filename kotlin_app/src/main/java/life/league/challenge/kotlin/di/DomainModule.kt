d12c277ac6ab29544d18c051821573dc8316c892
Caique Maurano <mauranocaique@gmail.com>
2021-10-24 03:41:54 -0300

    refactor di modules

---
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/di/DomainModule.kt
@@ -0,0 +1,27 @@
+package life.league.challenge.kotlin.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import life.league.challenge.kotlin.data.api.Api
+import life.league.challenge.kotlin.data.repositories.LoginRepositoryImpl
+import life.league.challenge.kotlin.data.repositories.PostRepositoryImpl
+import life.league.challenge.kotlin.data.repositories.UserRepositoryImpl
+import life.league.challenge.kotlin.domain.LoginRepository
+import life.league.challenge.kotlin.domain.PostRepository
+import life.league.challenge.kotlin.domain.UserRepository
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object DomainModule {
+
+    @Provides
+    fun provideLoginRepository(service: Api): LoginRepository = LoginRepositoryImpl(service)
+
+    @Provides
+    fun provideUserRepository(service: Api): UserRepository = UserRepositoryImpl(service)
+
+    @Provides
+    fun providePostRepository(service: Api): PostRepository = PostRepositoryImpl(service)
+}
--- kotlin_app/src/main/java/life/league/challenge/kotlin/di/NetworkModule.kt
+++ kotlin_app/src/main/java/life/league/challenge/kotlin/di/NetworkModule.kt
@@ -3,16 +3,9 @@ package life.league.challenge.kotlin.di
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.components.SingletonComponent
 import life.league.challenge.kotlin.BuildConfig
 import life.league.challenge.kotlin.data.api.Api
-import life.league.challenge.kotlin.data.repositories.LoginRepositoryImpl
-import life.league.challenge.kotlin.data.repositories.PostRepositoryImpl
-import life.league.challenge.kotlin.data.repositories.UserRepositoryImpl
-import life.league.challenge.kotlin.domain.LoginRepository
-import life.league.challenge.kotlin.domain.PostRepository
-import life.league.challenge.kotlin.domain.UserRepository
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -51,18 +44,4 @@ object NetworkModule {
         .build()
         .create(Api::class.java)
 
-}
-
-@Module
-@InstallIn(ViewModelComponent::class)
-object DomainModule {
-
-    @Provides
-    fun provideLoginRepository(service: Api): LoginRepository = LoginRepositoryImpl(service)
-
-    @Provides
-    fun provideUserRepository(service: Api): UserRepository = UserRepositoryImpl(service)
-
-    @Provides
-    fun providePostRepository(service: Api): PostRepository = PostRepositoryImpl(service)
 }
