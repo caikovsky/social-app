545e8183dee4bd3956b61caba6cf7c85d67ce27c
Caique Maurano <mauranocaique@gmail.com>
2021-10-24 22:45:59 -0300

    create flow observer for tests

---
+++ kotlin_app/src/test/java/life/league/challenge/kotlin/util/FlowTestObserver.kt
@@ -0,0 +1,50 @@
+package life.league.challenge.kotlin.util
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import org.junit.Assert.assertEquals
+import java.io.Closeable
+
+fun <T> Flow<T>.test(scope: CoroutineScope): FlowTestObserver<T> {
+    return FlowTestObserver(scope, this)
+}
+
+class FlowTestObserver<T> internal constructor(
+    scope: CoroutineScope,
+    flow: Flow<T>,
+) : Closeable {
+
+    private val _values = mutableListOf<T>()
+    val values: List<T>
+        get() = _values
+
+    private val job: Job = scope.launch {
+        flow.collect { _values.add(it) }
+    }
+
+    fun assertNoValues(): FlowTestObserver<T> {
+        assertEquals(emptyList<T>(), _values)
+        return this
+    }
+
+    fun assertValues(vararg values: T): FlowTestObserver<T> {
+        assertEquals(values.toList(), _values)
+        return this
+    }
+
+    fun assertValueCount(count: Int): FlowTestObserver<T> {
+        assertEquals(count, _values.size)
+        return this
+    }
+
+    fun finish() {
+        close()
+    }
+
+    override fun close() {
+        job.cancel()
+    }
+}
