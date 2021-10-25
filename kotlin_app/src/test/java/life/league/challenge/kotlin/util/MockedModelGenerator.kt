ea46cbb664f0b6ee2f3df4f837cf2fb7952c0595
Caique Maurano <mauranocaique@gmail.com>
2021-10-24 22:48:22 -0300

    refactor the mocked model generator

--- kotlin_app/src/test/java/life/league/challenge/kotlin/util/MockedModelGenerator.kt
+++ kotlin_app/src/test/java/life/league/challenge/kotlin/util/MockedModelGenerator.kt
@@ -1,61 +1,25 @@
 package life.league.challenge.kotlin.util
 
-import life.league.challenge.kotlin.data.model.*
+import life.league.challenge.kotlin.domain.model.AccountDomain
 import life.league.challenge.kotlin.domain.model.PostDomain
 import life.league.challenge.kotlin.domain.model.PostPerUserDomain
+import life.league.challenge.kotlin.domain.model.UserDomain
+import life.league.challenge.kotlin.ui.model.Post
 
 object MockedModelGenerator {
-    fun getAccountResponse(token: String? = "apiKey"): AccountResponse = AccountResponse(
-        apiKey = token
-    )
+    fun getAccountDomain(token: String = "apiKey"): AccountDomain =
+        AccountDomain(
+            apiKey = token
+        )
 
-    fun getUserResponseList(): List<UserResponse> = listOf(
-        getUserResponse(),
-        getUserResponse(id = 2, name = "Maxwell")
-    )
-
-    fun getUserResponse(
+    fun getUserDomain(
         id: Int = 1,
         name: String = "John Doe",
-    ): UserResponse = UserResponse(
+        thumbnail: String = "thumbnail_url",
+    ): UserDomain = UserDomain(
         id = id,
-        avatar = AvatarResponse(large = "large_url", medium = "medium_url", thumbnail = "thumbnail_url"),
         name = name,
-        username = "johndoe",
-        email = "johndoe@email.com",
-        address = AddressResponse(
-            street = "street",
-            suite = "suite",
-            city = "city",
-            zipcode = "zipcode",
-            geoResponse = GeoResponse(
-                lat = 0.0,
-                lng = 0.0
-            )
-        ),
-        phone = "12345",
-        website = "http://website.com",
-        company = CompanyResponse(
-            name = "name",
-            catchPhrase = "catchphrase",
-            bs = "bs"
-        )
-    )
-
-    fun getPostResponse(
-        id: Int = 1,
-        title: String = "title",
-        body: String = "body",
-    ): PostResponse = PostResponse(
-        userId = 0,
-        id = id,
-        title = title,
-        body = body
-    )
-
-    fun getPostResponseList(): List<PostResponse> = listOf(
-        getPostResponse(),
-        getPostResponse(id = 2),
+        thumbnail = thumbnail
     )
 
     fun getPostDomain(
@@ -68,16 +32,11 @@ object MockedModelGenerator {
         body = body
     )
 
-    fun getPostDomainList(): List<PostDomain> = listOf(
-        getPostDomain(),
-        getPostDomain(id = 2)
-    )
-
     fun getPostPerUserDomain(
         userId: Int = 1,
         name: String = "John Doe",
         thumbnail: String = "thumbnail_url",
-        posts: List<PostDomain> = getPostDomainList(),
+        posts: List<PostDomain> = listOf(getPostDomain()),
     ): PostPerUserDomain = PostPerUserDomain(
         userId = userId,
         name = name,
@@ -85,8 +44,13 @@ object MockedModelGenerator {
         posts = posts
     )
 
-    fun getPostPerUserDomainList(): List<PostPerUserDomain> = listOf(
-        getPostPerUserDomain(),
-        getPostPerUserDomain(userId = 2, name = "Maxwell")
+    fun getPostPerUserDomainList(): List<PostPerUserDomain> = listOf(getPostPerUserDomain())
+
+    fun getPost(): Post = Post(
+        userId = 1,
+        name = "John Doe",
+        thumbnail = "thumbnail_url",
+        title = "title",
+        body = "body"
     )
 }
