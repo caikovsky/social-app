1dbd8eb196e7d7fb0bb547f1bf8c79cf83f15bdb
Caique Maurano <mauranocaique@gmail.com>
2021-10-24 22:48:02 -0300

    refactor test extensions

--- kotlin_app/src/test/java/life/league/challenge/kotlin/util/TestExtensions.kt
+++ kotlin_app/src/test/java/life/league/challenge/kotlin/util/TestExtensions.kt
@@ -1,7 +1,5 @@
 package life.league.challenge.kotlin.util
 
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import org.junit.Assert
 import java.io.FileNotFoundException
 import java.net.URL
@@ -11,12 +9,5 @@ fun readFile(path: String): String {
 
     return content?.readText() ?: throw FileNotFoundException("file path: $path was not found")
 }
-
-inline fun <reified T> parse(gson: Gson, path: String): T {
-    val file = readFile("$path.json")
-    return gson.fromJson(file, object : TypeToken<T>() {}.type)
-}
-
-inline fun <reified T> Gson.parseFrom(path: String): T = parse(this, path)
 
 infix fun Any.shouldBe(expected: Any) = Assert.assertEquals(expected, this)
