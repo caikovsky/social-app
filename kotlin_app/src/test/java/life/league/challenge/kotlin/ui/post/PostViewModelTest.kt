d24a510a972707309bb05a58fdcc04de100fdabf
Caique Maurano <mauranocaique@gmail.com>
2021-10-24 22:48:30 -0300

    refactor viewmodel test

--- kotlin_app/src/test/java/life/league/challenge/kotlin/ui/post/PostViewModelTest.kt
+++ kotlin_app/src/test/java/life/league/challenge/kotlin/ui/post/PostViewModelTest.kt
@@ -1,18 +1,14 @@
 package life.league.challenge.kotlin.ui.post
 
-import app.cash.turbine.test
-import io.mockk.coEvery
-import io.mockk.mockk
-import io.mockk.unmockkAll
+import android.util.Log
+import io.mockk.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import life.league.challenge.kotlin.domain.PostsPerUserUseCase
-import life.league.challenge.kotlin.domain.model.toViewEntity
 import life.league.challenge.kotlin.util.MainCoroutineRule
 import life.league.challenge.kotlin.util.MockedModelGenerator
-import life.league.challenge.kotlin.util.shouldBe
+import life.league.challenge.kotlin.util.test
 import org.junit.After
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import kotlin.time.ExperimentalTime
@@ -23,13 +19,8 @@ class PostViewModelTest {
     @get:Rule
     var mainCoroutineRule = MainCoroutineRule()
 
-    private lateinit var viewModel: PostViewModel
     private val postsPerUserUseCase: PostsPerUserUseCase = mockk()
-
-    @Before
-    fun setUp() {
-        viewModel = PostViewModel(postsPerUserUseCase)
-    }
+    private val viewModel = PostViewModel(postsPerUserUseCase)
 
     @After
     fun tearDown() {
@@ -38,28 +29,19 @@ class PostViewModelTest {
 
     @ExperimentalTime
     @Test
-    fun `loading as default value`() {
+    fun `successfully retrieve the posts per user when initialize event is triggered`() {
         mainCoroutineRule.runBlockingTest {
-            viewModel.state.test {
-                awaitItem() shouldBe PostViewModel.State.Loading
-            }
-        }
-    }
+            val useCaseResponse = MockedModelGenerator.getPostPerUserDomainList()
 
-    @ExperimentalTime
-    @Test
-    fun `successfully retrieve the posts per user when initilize event is triggered`() {
-        mainCoroutineRule.runBlockingTest {
-            val expected = MockedModelGenerator.getPostPerUserDomainList().map {
-                it.toViewEntity()
-            }
+            coEvery { postsPerUserUseCase(any() as String, any() as String) } returns useCaseResponse
 
-            coEvery { postsPerUserUseCase(any() as String, any() as String) } returns MockedModelGenerator.getPostPerUserDomainList()
+            val expected = listOf(MockedModelGenerator.getPost())
+            val observer = viewModel.state.test(this)
+
             viewModel.onEvent(PostViewModel.UiEvent.Initialize)
 
-            viewModel.state.test {
-                awaitItem() shouldBe PostViewModel.State.Content(expected)
-            }
+            observer.assertValues(PostViewModel.State.Loading, PostViewModel.State.Content(expected)).finish()
+            observer.assertValueCount(2)
         }
     }
 
@@ -67,16 +49,16 @@ class PostViewModelTest {
     @Test
     fun `successfully retrieve the posts per user when refresh event is triggered`() {
         mainCoroutineRule.runBlockingTest {
-            val expected = MockedModelGenerator.getPostPerUserDomainList().map {
-                it.toViewEntity()
-            }
+            val useCaseResponse = MockedModelGenerator.getPostPerUserDomainList()
 
-            coEvery { postsPerUserUseCase(any() as String, any() as String) } returns MockedModelGenerator.getPostPerUserDomainList()
+            coEvery { postsPerUserUseCase(any() as String, any() as String) } returns useCaseResponse
+
+            val expected = listOf(MockedModelGenerator.getPost())
             viewModel.onEvent(PostViewModel.UiEvent.Refresh)
 
-            viewModel.state.test {
-                awaitItem() shouldBe PostViewModel.State.Content(expected)
-            }
+            val observer = viewModel.state.test(this)
+
+            observer.assertValues(PostViewModel.State.Content(expected)).finish()
         }
     }
 
@@ -84,13 +66,14 @@ class PostViewModelTest {
     @Test
     fun `error state when something goes wrong`() {
         mainCoroutineRule.runBlockingTest {
-            coEvery { postsPerUserUseCase(any() as String, any() as String) } throws Exception()
+            mockkStatic(Log::class)
+            every { Log.e(any() as String, any() as String) } returns 1
+            coEvery { postsPerUserUseCase(any() as String, any() as String) } throws Exception("")
 
             viewModel.onEvent(PostViewModel.UiEvent.Initialize)
+            val observer = viewModel.state.test(this)
 
-            viewModel.state.test {
-                awaitItem() shouldBe PostViewModel.State.Error
-            }
+            observer.assertValues(PostViewModel.State.Error).finish()
         }
     }
 
