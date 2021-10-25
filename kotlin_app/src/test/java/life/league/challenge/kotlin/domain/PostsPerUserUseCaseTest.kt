698eb1814cd1afb49a023d1e9826359b3e54abbb
Caique Maurano <mauranocaique@gmail.com>
2021-10-24 22:48:13 -0300

    add usecase test

--- kotlin_app/src/test/java/life/league/challenge/kotlin/domain/PostsPerUserUseCaseTest.kt
+++ kotlin_app/src/test/java/life/league/challenge/kotlin/domain/PostsPerUserUseCaseTest.kt
@@ -7,6 +7,9 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import life.league.challenge.kotlin.data.model.PostResponse
 import life.league.challenge.kotlin.data.model.UserResponse
+import life.league.challenge.kotlin.domain.repositories.LoginRepository
+import life.league.challenge.kotlin.domain.repositories.PostRepository
+import life.league.challenge.kotlin.domain.repositories.UserRepository
 import life.league.challenge.kotlin.util.MainCoroutineRule
 import life.league.challenge.kotlin.util.MockedModelGenerator
 import life.league.challenge.kotlin.util.shouldBe
@@ -21,15 +24,10 @@ class PostsPerUserUseCaseTest {
     @get:Rule
     var mainCoroutineRule = MainCoroutineRule()
 
-    private lateinit var postsPerUserUseCase: PostsPerUserUseCase
     private val loginRepository: LoginRepository = mockk()
     private val userRepository: UserRepository = mockk()
     private val postRepository: PostRepository = mockk()
-
-    @Before
-    fun setUp() {
-        postsPerUserUseCase = PostsPerUserUseCase(loginRepository, userRepository, postRepository)
-    }
+    private val postsPerUserUseCase = PostsPerUserUseCase(loginRepository, userRepository, postRepository)
 
     @After
     fun tearDown() {
@@ -40,37 +38,16 @@ class PostsPerUserUseCaseTest {
     fun `return a list of posts per user successfully`() {
         val name = "name"
         val password = "password"
-        val token = MockedModelGenerator.getAccountResponse()
-        val users: List<UserResponse> = MockedModelGenerator.getUserResponseList()
-        val posts: List<PostResponse> = MockedModelGenerator.getPostResponseList()
+        val token = MockedModelGenerator.getAccountDomain()
+        val users = listOf(MockedModelGenerator.getUserDomain())
+        val posts = listOf(MockedModelGenerator.getPostDomain())
 
         mainCoroutineRule.runBlockingTest {
             val expected = MockedModelGenerator.getPostPerUserDomainList()
 
             coEvery { loginRepository(name, password) } returns token
-            coEvery { userRepository(token.apiKey ?: "") } returns users
-            coEvery { postRepository(token.apiKey ?: "", any() as Int) } returns posts
-
-            val result = postsPerUserUseCase(name, password)
-
-            expected shouldBe result
-        }
-    }
-
-    @Test
-    fun `login repository fails to create a token and returns null`() {
-        val name = "name"
-        val password = "password"
-        val token = MockedModelGenerator.getAccountResponse(null)
-        val users: List<UserResponse> = MockedModelGenerator.getUserResponseList()
-        val posts: List<PostResponse> = MockedModelGenerator.getPostResponseList()
-
-        runBlockingTest {
-            val expected = MockedModelGenerator.getPostPerUserDomainList()
-
-            coEvery { loginRepository(name, password) } returns token
-            coEvery { userRepository(token.apiKey ?: "") } returns users
-            coEvery { postRepository(token.apiKey ?: "", any() as Int) } returns posts
+            coEvery { userRepository(token.apiKey) } returns users
+            coEvery { postRepository(token.apiKey, any() as Int) } returns posts
 
             val result = postsPerUserUseCase(name, password)
 
